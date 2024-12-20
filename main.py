9b7e3d29498c50ef25c81de695b153b192d3ca0f
ValmeI <Ignarvalme@gmail.com>
2024-12-20 14:03:43 +0200

    Commented out FastAPI import; updated dependencies in requirements.txt.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from backend.src.search_api import app as search_app  # Import FastAPI objekt teisest failist
+#from fastapi import FastAPI
+from backend.src.search_api import app as search_app  # Import FastAPI pbject from search_api
 
 # Kui soovid `app` defineerida otse:
 # app = FastAPI()
