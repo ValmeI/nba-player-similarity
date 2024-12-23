7eea7ce9dfe48bf72c4f0083cf888890f95176ea
ValmeI <ignarvalme@gmail.com>
2024-12-23 20:12:12 +0200

    Enable data fetching, add error check, and update dependencies in requirements.txt

--- data/data_main.py
+++ data/data_main.py
@@ -8,8 +8,9 @@ from backend.config import settings
 
 
 if __name__ == "__main__":
+    print("Starting data fetching process...")
     # NB! Next part is about fetching all players raw data
-    FETCH_RAW_DATA_FETCH = False
+    FETCH_RAW_DATA_FETCH = True
 
     if FETCH_RAW_DATA_FETCH:
         start_time = time.perf_counter()
@@ -18,10 +19,14 @@ if __name__ == "__main__":
         logger.info(
             f"Finished NBA raw data fetching process on {datetime.now()} and it took {time.perf_counter() - start_time:.2f} seconds"
         )
+        
+    if not os.path.exists(settings.RAW_NBA_DATA_PATH):
+        logger.error(f"Raw NBA data folder {settings.RAW_NBA_DATA_PATH} does not exist.")
+        exit(1)
 
     # NB! Next part is about processing all players metrics
     PROCESS_ALL_PLAYERS_METRIC = True  # In case u want to process all players metrics again
     OWERWRITE_PLAYER_METRICS_IF_EXISTS = True
 
-    if PROCESS_ALL_PLAYERS_METRIC and os.path.exists(settings.RAW_NBA_DATA_PATH):
+    if PROCESS_ALL_PLAYERS_METRIC:
         process_player_metrics_in_threads(OWERWRITE_PLAYER_METRICS_IF_EXISTS)
