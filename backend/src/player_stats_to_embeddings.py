792484e728b4606e3fbfeb7267c1146031177ee0
ValmeI <ignarvalme@gmail.com>
2024-12-25 15:39:00 +0200

    normalize only seperate columns not originals

--- backend/src/player_stats_to_embeddings.py
+++ backend/src/player_stats_to_embeddings.py
@@ -26,8 +26,9 @@ NUMERIC_COLUMNS = [
     "PTS Responsibility",  # Points responsibility
 ]
 
-
-NEW_NORMALIZED_COLUMNS = [f"NORM_{col}" for col in NUMERIC_COLUMNS]
+# Define the normalized columns as we want to keep original columns aslo
+NORMALIZED_COLUMN_PREFIX = "NORM_"
+TO_NORMALIZED_COLUMNS = [f"{NORMALIZED_COLUMN_PREFIX}{col}" for col in NUMERIC_COLUMNS]
 
 METADATA_COLUMNS = ["SEASON_ID", "PLAYER_NAME"]
 
@@ -42,11 +43,23 @@ COLUMN_WEIGHTS = {
     "BLK_PER_GAME": 1.5,  # Defense
 }
 
-# TODO: check if this is needed
+# TODO: check if those two are needed
 def apply_weights(player_stats_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Add weights to only the normalized columns and not the original ones
+    as we want to keep the original columns for user return, for examples and reference
+    """
     for column, weight in COLUMN_WEIGHTS.items():
-        if column in player_stats_df.columns:
-            player_stats_df[column] *= weight
+        new_normalized_column = f"{NORMALIZED_COLUMN_PREFIX}{column}"
+        if new_normalized_column in player_stats_df.columns:
+            player_stats_df[new_normalized_column] *= weight
+    return player_stats_df
+
+def remove_weight(player_stats_df: pd.DataFrame) -> pd.DataFrame:
+    for column, weight in COLUMN_WEIGHTS.items():
+        new_normalized_column = f"{NORMALIZED_COLUMN_PREFIX}{column}"
+        if new_normalized_column in player_stats_df.columns:
+            player_stats_df[new_normalized_column] /= weight
     return player_stats_df
 
 
@@ -54,14 +67,14 @@ def fill_missing_values(player_stats_df: pd.DataFrame) -> pd.DataFrame:
     """
     Fill missing values in numeric columns with 0 as those are really old players
     """
-    player_stats_df[NUMERIC_COLUMNS] = player_stats_df[NUMERIC_COLUMNS].fillna(0).infer_objects(copy=False)
+    player_stats_df[NUMERIC_COLUMNS] = player_stats_df[NUMERIC_COLUMNS].fillna(0)
     return player_stats_df
 
 
 def normalize_features(player_stats_df: pd.DataFrame) -> pd.DataFrame:
     scaler = StandardScaler()
     # to keep original columns aslo, so we could return those to user after
-    player_stats_df[NEW_NORMALIZED_COLUMNS] = scaler.fit_transform(player_stats_df[NUMERIC_COLUMNS])
+    player_stats_df[TO_NORMALIZED_COLUMNS] = scaler.fit_transform(player_stats_df[NUMERIC_COLUMNS])
     return player_stats_df
 
 
@@ -74,7 +87,7 @@ def create_season_embeddings(player_stats_df: pd.DataFrame) -> pd.DataFrame:
     player_stats_df = fill_missing_values(player_stats_df)
     player_stats_df = apply_weights(player_stats_df)
     player_stats_df = normalize_features(player_stats_df)
-    player_stats_df["embeddings"] = player_stats_df[NEW_NORMALIZED_COLUMNS].apply(
+    player_stats_df["embeddings"] = player_stats_df[TO_NORMALIZED_COLUMNS].apply(
         lambda row: row.to_numpy().tolist(), axis=1
     )
     logger.info(
